a61b0758c1bf6ea0b2e24da2319a0b852e2836ae
Marius Mikučionis <marius@cs.aau.dk>
2025-07-30 16:33:32 +0200

    Added CMake installation script for Catch2 unit testing framework

---
+++ catch2.cmake
@@ -0,0 +1,35 @@
+# Downloads and compiles Catch2 unit testing framework
+find_package(Catch2 QUIET)
+if(Catch2_FOUND)
+    message(STATUS "Found Catch2: ${Catch2_SOURCE_DIR}")
+else(Catch2_FOUND)
+    message(STATUS "Failed to find Catch2, going to fetch from source")
+    include(FetchContent)
+    set(FETCHCONTENT_QUIET ON)
+    set(FETCHCONTENT_UPDATES_DISCONNECTED ON)
+
+    set(CATCH_INSTALL_DOCS OFF CACHE BOOL  "Install documentation alongside library")
+    set(CATCH_INSTALL_EXTRAS ON CACHE BOOL "Install extras (CMake scripts, debugger helpers) alongside library")
+    set(CATCH_DEVELOPMENT_BUILD OFF CACHE BOOL "Build tests, enable warnings, enable Werror, etc")
+    set(CATCH_ENABLE_REPRODUCIBLE_BUILD ON CACHE BOOL  "Add compiler flags for improving build reproducibility")
+
+    set(CATCH_BUILD_TESTING OFF CACHE BOOL "Build the SelfTest project")
+    set(CATCH_BUILD_EXAMPLES OFF CACHE BOOL "Build code examples")
+    set(CATCH_BUILD_EXTRA_TESTS OFF CACHE BOOL "Build extra tests")
+    set(CATCH_BUILD_FUZZERS OFF CACHE BOOL "Build fuzzers")
+    set(CATCH_ENABLE_COVERAGE OFF CACHE BOOL "Generate coverage for codecov.io")
+    set(CATCH_ENABLE_WERROR OFF CACHE BOOL "Enables Werror during build")
+    set(CATCH_BUILD_SURROGATES OFF CACHE BOOL "Enable generating and building surrogate TUs for the main headers")
+    set(CATCH_ENABLE_CONFIGURE_TESTS OFF CACHE BOOL "Enable CMake configuration tests. WARNING: VERY EXPENSIVE")
+    set(CATCH_ENABLE_CMAKE_HELPER_TESTS OFF CACHE BOOL "Enable CMake helper tests. WARNING: VERY EXPENSIVE")
+
+    FetchContent_Declare(Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG v3.9.0    # "devel" for latest
+        GIT_SHALLOW TRUE  # download specific revision only (git clone --depth 1)
+        GIT_PROGRESS TRUE # show download progress in Ninja
+        FIND_PACKAGE_ARGS NAMES Catch2
+        USES_TERMINAL_DOWNLOAD TRUE)
+    FetchContent_MakeAvailable(Catch2)
+    message(STATUS "Fetched Catch2: ${Catch2_SOURCE_DIR}")
+endif (Catch2_FOUND)
